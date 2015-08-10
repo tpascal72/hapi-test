c3a944b54b5a1a8d5c906d6c708f3922d8f1bd11
David Mendez <desiratech@gmail.com>
2015-08-10 12:54:29 -0500

    added api loader

---
+++ src/api/loader.js
@@ -0,0 +1,19 @@
+var server = require('../server');
+var path = require('path');
+
+//Load items api
+require('./items/routes');
+
+var staticPath = path.join(path.resolve(__dirname, "../../", "front-end"));
+
+var staticRoute = {
+	method: "GET",
+	path: "/{param}",
+	handler: {
+		directory: {
+			path: staticPath
+		}
+	}
+};
+
+server.route(staticRoute);
