f77748fb85995d45adddaad45e8c266fd84635c3
David Mendez <desiratech@gmail.com>
2015-08-10 12:10:37 -0500

    created read route

---
+++ api/items/read.js
@@ -0,0 +1,14 @@
+var db = require('../../store/db');
+var Promise = require('bluebird');
+
+function get(criteria){
+	//select all items from items table
+	var request = db("items").select();
+	if (criteria)
+		request
+			.where(criteria)
+			.then(function (items) {return Promise.resolve(items[0] ); });
+	return request;
+}
+
+module.exports  = get;
